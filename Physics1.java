761be268b809212739d30b2f670d9eaf59b0014d
aturn042 <aturn042@ucr.edu>
2024-10-13 17:56:14 -0700

    Update Physics1.java
    Fixed issues with math

--- Physics1.java
+++ Physics1.java
@@ -9,17 +9,17 @@ public class Physics1 {
     public static void main(String [] physics){
         Scanner scanner = new Scanner(System.in);
         //input your position and time data into the position and time arrays below
-        double[] position = {150,120,100,70,50,30};
-        double[] time = {0.55,0.5,0.45,0.38,0.32,0.25};
+        double[] position = {150,120,100,70,50,30,0};
+        double[] time = {0.55,0.5,0.45,0.38,0.32,0.25,0};
         //
-        double[] gList = {0,0,0,0,0};
-        double[]gUncert = {0,0,0,0,0};
+        double[] gList = {0,0,0,0,0,0};
+        double[]gUncert = {0,0,0,0,0,0};
         System.out.println("Enter height uncertainty");
         double uncert = scanner.nextDouble();
 
-        for(int i = 0; i<5;i++){
+        for(int i = 0; i<position.length-1;i++){
             gList[i] = (2*position[i])/(time[i]*time[i]);
-            gUncert[i] = uncert/position[i];
+            gUncert[i] = Math.sqrt(uncert/position[i]);
         }
         System.out.println("G Values");
         for(int i = 0; i<5; i++){
@@ -30,5 +30,9 @@ public class Physics1 {
         for(int i = 0; i<5;i++) {
             System.out.print(gUncert[i]+", ");
         }
+        double lastG = gList[5] = (2*position[5])/(time[5]*time[5]);
+        double lastGuncert = Math.sqrt(uncert/position[5]);
+        System.out.println();
+        System.out.println("Extra value: "+lastG+" Extra uncertainty: "+lastGuncert);
     }
 }
